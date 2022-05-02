c2265d21da011e77d0425e6b3da20477354adb81
Witherking25 <witherking@withertech.com>
2022-05-02 14:17:42 -0500

    remove upload.sh

--- upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo flatpak-builder --install-deps-from=flathub --force-clean --gpg-sign=597347D1434D8E68B3D72E71A892A7123B116051 --repo=repo builddir info.cemu.Cemu.json
-flat-manager-client push --commit --publish $(flat-manager-client create https://repo.withertech.com/flatpak/api/ main) repo/
